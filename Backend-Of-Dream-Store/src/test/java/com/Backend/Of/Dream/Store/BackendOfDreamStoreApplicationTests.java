51d7d115d7fffbf8ca1520af97a1575df7e396aa
NTNET\nikitpat <nikita.patil@amdocs.com>
2023-02-03 19:36:29 +0530

    done

---
+++ Backend-Of-Dream-Store/src/test/java/com/Backend/Of/Dream/Store/BackendOfDreamStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.Of.Dream.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOfDreamStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
