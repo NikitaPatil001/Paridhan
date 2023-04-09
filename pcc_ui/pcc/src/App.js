ee905fd5a8f61d32eb0d514d5686fc98f1fea1f5
mahendra kolhe <mkolhe23@gmail.com>
2023-04-10 00:17:45 +0530

    paridhan calculator

---
+++ pcc_ui/pcc/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Sell from './Component/Sell';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Sell></Sell>
+      </header>
+    </div>
+  );
+}
+
+export default App;
